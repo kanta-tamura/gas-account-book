c473a6116dce519e80a668f694e90cccc9fbc1bf
Kanta Tamura <kanta03085@gmail.com>
2021-01-11 23:00:31 +0900

    fix: header

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <!-- tool bar -->
+    <v-app-bar app color="green" dark>
+      <!-- title -->
+      <v-toolbar-title>GAS 家計簿</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <!-- table icon button -->
+      <v-btn icon to="/">
+        <v-icon>mdi-file-table-outline</v-icon>
+      </v-btn>
+      <!-- gear icon button -->
+      <v-btn icon to="/setting">
+        <v-icon>mdi-cog</v-icon>
       </v-btn>
     </v-app-bar>
-
+    <!-- main contents -->
     <v-main>
-      <HelloWorld/>
+      <v-container fluid>
+        <!-- router-view の中身がパスによって切り替わる -->
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  name: 'App'
+}
 </script>
